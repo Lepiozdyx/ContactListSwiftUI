280e814c5a6018d668e4d3845685f13d2f620b7a
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-23 16:53:34 -0300

    Add ContactListView + ContactDetailsView

---
+++ ContactListSwiftUI/Views/ContactDetailsView.swift
@@ -0,0 +1,48 @@
+//
+//  ContactDetailsView.swift
+//  ContactListSwiftUI
+//
+//  Created by Alex on 23.06.2023.
+//
+
+import SwiftUI
+
+struct ContactDetailsView: View {
+    let person: Person
+    
+    var body: some View {
+        List {
+            HStack {
+                Spacer()
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Spacer()
+            }
+            RowView(image: "phone", text: person.phone)
+            RowView(image: "tray", text: person.email)
+        }
+        .navigationTitle(person.fullName)
+    }
+}
+
+struct ContactDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactDetailsView(person: Person(name: "", surname: "", phone: "3333-333", email: "luis@gmail.com ", icon: ""))
+    }
+}
+
+// MARK: - RowView
+struct RowView: View {
+    let image: String
+    let text: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .frame(width: 30, alignment: .leading)
+                .foregroundColor(.blue)
+            Text(text)
+        }
+    }
+}
---
+++ ContactListSwiftUI/Views/ContactListView.swift
@@ -0,0 +1,30 @@
+//
+//  ContactListView.swift
+//  ContactListSwiftUI
+//
+//  Created by Alex on 23.06.2023.
+//
+
+import SwiftUI
+
+struct ContactListView: View {
+    let persons: [Person]
+    
+    var body: some View {
+        NavigationStack {
+            List(persons, id: \.fullName) { person in
+                NavigationLink(destination: ContactDetailsView(person: person)) {
+                    Text(person.fullName)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("Contact List")
+        }
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactListView(persons: Person.getRandomPersons())
+    }
+}
