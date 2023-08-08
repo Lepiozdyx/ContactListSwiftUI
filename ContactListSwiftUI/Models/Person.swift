21e4fc071fe2a3ecd5016f6df639bc6d20d8cca5
Alex Zhulai <Lepiozdyx@gmail.com>
2023-08-08 15:13:44 -0300

    Add new model(Team) + new data store info

--- ContactListSwiftUI/Models/Person.swift
+++ ContactListSwiftUI/Models/Person.swift
@@ -10,9 +10,10 @@ import Foundation
 struct Person: Identifiable {
     let name: String
     let surname: String
-    let team: String
     let email: String
     let icon: String
+    let number: String
+    let team: String
     let id = UUID()
     
     var fullName: String {
@@ -24,22 +25,65 @@ struct Person: Identifiable {
         
         let names = DataStore.shared.names
         let surnames = DataStore.shared.surnames
-        let teams = DataStore.shared.teams
         let emails = DataStore.shared.emails
         let icons = DataStore.shared.icons
+        let numbers = DataStore.shared.numbers
+        let teams = DataStore.shared.teams
         
         for index in 0..<names.count {
             let person = Person(
                 name: names[index],
                 surname: surnames[index],
-                team: teams[index],
                 email: emails[index],
-                icon: icons[index]
+                icon: icons[index],
+                number: numbers[index],
+                team: teams[index]
             )
             
             uniquePersons.append(person)
         }
         
         return uniquePersons
+    }
+}
+
+struct Team: Identifiable {
+    let name: String
+    let logo: String
+    let head: String
+    let foundationYear: String
+    let country: String
+    let flag: String
+    let bolid: String
+    let bolidIcon: String
+    let id = UUID()
+    
+    static func getTeam() -> [Team] {
+        var uniqueTeam: [Team] = []
+        
+        let names = DataStore.shared.teamNames
+        let logos = DataStore.shared.teamLogos
+        let heads = DataStore.shared.teamHeads
+        let foundationYears = DataStore.shared.teamFoundationYears
+        let countries = DataStore.shared.teamCountries
+        let flags = DataStore.shared.countryFlags
+        let bolides = DataStore.shared.bolidNames
+        let bolidIcons = DataStore.shared.bolidIcons
+        
+        for index in 0..<names.count {
+            let team = Team(
+                name: names[index],
+                logo: logos[index],
+                head: heads[index],
+                foundationYear: foundationYears[index],
+                country: countries[index],
+                flag: flags[index],
+                bolid: bolides[index],
+                bolidIcon: bolidIcons[index]
+            )
+            uniqueTeam.append(team)
+        }
+        
+        return uniqueTeam
     }
 }
--- ContactListSwiftUI/Services/DataStore.swift
+++ ContactListSwiftUI/Services/DataStore.swift
@@ -12,11 +12,29 @@ final class DataStore {
 
     let surnames = ["Hamilton", "Verstappen", "Bottas", "Norris", "Perez", "Ricciardo", "Leclerc", "Gasly", "Ocon", "Sainz", "Stroll", "Alonso", "Sargeant", "Tsunoda", "Russell", "Magnussen", "Albon", "Hulkenberg", "Piastri", "Guanyu"]
 
-    let teams = ["Mercedes", "Red Bull Racing", "Alfa Romeo", "McLaren", "Red Bull Racing", "AlphaTauri", "Ferrari", "Alpine", "Alpine", "Ferrari", "Aston Martin Racing", "Aston Martin Racing", "Williams", "AlphaTauri", "Mercedes", "Haas F1 Team", "Williams", "Haas F1 Team", "McLaren", "Alfa Romeo"]
-
     let emails = ["mercedes@example.com", "redbull@example.com", "alfaromeo@example.com", "mclaren@example.com", "redbull-2@example.com", "alphatauri-2@example.com", "ferrari@example.com", "alpine@example.com", "alpine-2@example.com", "ferrari-2@example.com", "astonmartin-2@example.com", "astonmartin@example.com", "williams-2@example.com", "alphatauri@example.com", "mercedes-2@example.com", "haas@example.com", "williams@example.com", "haas-2@example.com", "mclaren-2@example.com", "alfaromeo-2@example.com"]
     
     let icons = ["hamilton", "verstappen", "bottas", "norris", "perez", "ricciardo", "leclerc", "gasly", "ocon", "sainz", "stroll", "alonso", "sargeant", "tsunoda", "russell", "magnussen", "albon", "hulkenberg", "piastri", "guanyu"]
+    
+    let numbers = ["44", "1", "77", "4", "11", "3", "16", "10", "31", "55", "18", "14", "2", "22", "63", "20", "23", "27", "81", "24"]
+    
+    let teams = ["Mercedes", "Red Bull Racing", "Alfa Romeo", "McLaren", "Red Bull Racing", "AlphaTauri", "Ferrari", "Alpine", "Alpine", "Ferrari", "Aston Martin Racing", "Aston Martin Racing", "Williams", "AlphaTauri", "Mercedes", "Haas F1 Team", "Williams", "Haas F1 Team", "McLaren", "Alfa Romeo"]
+    
+    let teamNames = ["Mercedes", "Red Bull Racing", "Alfa Romeo", "McLaren", "AlphaTauri", "Ferrari", "Alpine", "Aston Martin Racing", "Williams", "Haas F1 Team"]
+    
+    let teamLogos = ["mercedes", "redbull", "alfaromeo", "mclaren", "alphatauri", "ferrari", "alpine", "aston", "williams", "haas"]
+    
+    let teamHeads = ["Toto Wolff", "Christian Horner", "Frédéric Vasseur", "Zak Brown", "Franz Tost", "Mattia Binotto", "Laurent Rossi", "Otmar Szafnauer", "Jost Capito", "Guenther Steiner"]
+    
+    let teamFoundationYears = ["1954", "2005", "1950", "1966", "1985", "1929", "1976", "1959", "1977", "1983"]
+    
+    let teamCountries = ["Germany", "Austria", "Switzerland", "UK", "Italy", "Italy", "France", "UK", "UK", "USA"]
+    
+    let countryFlags = ["Germany", "Austria", "Switzerland", "UK", "Italy", "Italy", "France", "UK", "UK", "USA"]
+    
+    let bolidNames = ["W14", "RB19", "C43", "MCL60", "AT04", "SF-23", "A523", "AMR23", "FW45", "VF-23"]
+    
+    let bolidIcons = ["W14", "RB19", "C43", "MCL60", "AT04", "SF-23", "A523", "AMR23", "FW45", "VF-23"]
 
     private init() {}
 }
